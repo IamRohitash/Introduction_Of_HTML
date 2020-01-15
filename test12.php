e0175752af872e0cb71a8f71ecff79b7b42c4dbd
Rohitash Yadav <rohitashyadav455@gmail.com>
2020-01-14 21:06:05 -0500

    this is my html code

---
+++ test12.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My first php page</h1>
+    <?php
+    echo "Hello world"
+    ?>
+</body>
+</html>
